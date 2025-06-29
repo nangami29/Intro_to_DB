490f50336e6b8e54fde7352296115160e0663529
nangami29 <rachaelnangami29@gmail.com>
2025-06-29 12:46:10 +0300

    insert

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO customer
  (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@gmail.com',  '123 Happiness Ave.')
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com',  '123 Happiness Ave.')
 
