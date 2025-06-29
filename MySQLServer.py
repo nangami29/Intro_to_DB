8ce33d06bb776fedb3dc6f73fc3bc1f5c056bf12
Nangami Rachael-Loureen <rachaelnangami29@gmail.com>
2025-06-29 15:01:47 +0300

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+mydb= mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='nangamira1',
+    
+)
+mycursor = mydb.cursor()
+
+#create database
+mycursor.execute("""
+CREATE DATABASE IF NOT EXISTS alx_book_store
+
+""")
+
+print("Database 'alx_book_store' created successfully!")
+
+# re-connect with the database created
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='nangami1',
+    database = 'alx_book_store'
+
+)
+if 'connected':
+    print(f"DB connected successfuly!")
+else:
+     print(f"Error: Failed to connect to the DB")
+          
+          
+#Execute sql statements using the execute() method on the cursor
+# close connection to the database
+mycursor.close()
+
+mydb.close()
+print(mydb.get_server_info())
